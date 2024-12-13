8d578345cfcfafbc99f70deaba5de31a7be00d85
Ethan Crawford <ethan.crawford5532@gmail.com>
2024-12-13 06:43:39 +0000

    additional config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+dotenv.config();
 
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    define: {
+      'process.env': process.env
+    }
+  }
 });
